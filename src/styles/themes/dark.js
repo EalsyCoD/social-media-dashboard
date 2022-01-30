ccb4346a522e554184f839ca71894a51d7f79066
EalsyCoD <ealsycod@gmail.com>
2022-01-30 16:38:35 +0300

    added header and switch

---
+++ src/styles/themes/dark.js
@@ -0,0 +1,12 @@
+export default {
+    title: 'dark',
+  
+    colors: {
+      bgToggle:
+        'linear-gradient( to right, hsl(210, 78%, 56%) 0%, hsl(146, 68%, 55%) 100% )',
+  
+      background: 'hsl(230, 17%, 14%)',
+      topBackgroun: 'hsl(232, 19%, 15%)',
+      text: '#fff',
+    },
+  }
