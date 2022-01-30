08fa39ebf7908beebb9270fb660a6c5352ee1222
EalsyCoD <ealsycod@gmail.com>
2022-01-30 16:46:38 +0300

    fixs styles header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,17 +2,21 @@ import styled from 'styled-components'
 
 
 export const Container = styled.header`
+& {
   background: ${(props) => props.theme.colors.topBackgroun};
   border-radius: 0 0 20px 20px;
-  padding: 40px 35px 100px 35px;
+  padding: 40px 35px 150px 35px;
+}
+@media (min-width: 768px) {
+  display: grid;
+  grid-template-columns: 1fr 150px;
+}
 `
 
 export const Wrapper = styled.div`
-  
+
 `
-export const Switch = styled.div `
- 
-`
+
 
 
 export const Title = styled.h1`
@@ -20,10 +24,16 @@ export const Title = styled.h1`
 `
 
 export const Label = styled.p`
+& {
   color: ${(props) => props.theme.colors.secondary};
-  font-size: 1rem;
+  font-size: 16px;
   font-weight: 700;
   letter-spacing: 0.7px;
   border-bottom: 0.6px solid ${(props) => props.theme.colors.secondary};
-  padding-bottom: 1.5em;
+  padding-bottom: 24px;
+}
+@media (min-width: 768px) {
+  border-bottom: none !important;
+  padding-bottom: 0 !important;
+}
 `
